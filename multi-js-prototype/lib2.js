d73f5f4e00388a271edaa430015f0cc81e0b7abc
Template99 <template99@comcast.net>
2023-01-06 21:15:51 -0600

    just check in already.

--- multi-js-prototype/lib2.js
+++ multi-js-prototype/lib2.js
@@ -1,9 +1,13 @@
 // just a second file
 // testing interopoobality
-var lib2Var = 0; 
+var lib2Var = 123; 
+
+console.log(`pre load lib2`); // does not work
 
 window.onload = function() {
-    console.log(`lib 2  initiated`); // not seeing it
+    console.log(`lib 2  initiated`); // works
+    //console.log(`value of local storage: ${window.localStorage.getItem("zonky")} `); // not working
+    console.log(`value of simple lib ${simpleLibVar}`);
 }
 
 function lib2_testfunction(){
--- multi-js-prototype/simpleLib.js
+++ multi-js-prototype/simpleLib.js
@@ -1,9 +1,11 @@
 // just a single library
 
-console.log('simplelib: preload script!');
+var simpleLibVar = 1; 
+
+console.log('simplelib: preload script!');  // not working
 
 window.onload = function() {
     console.log(`simple library initiated`);
-
-
+    //console.log(`value of local storage ${localStorage.getItem("zonky")} `);
+    console.log(`value of other libs var: ${lib2Var}`);
 }
